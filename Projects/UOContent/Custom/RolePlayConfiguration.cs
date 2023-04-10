f4e3b66336e1bdb4a3961a9fb9388d23c057fa31
Filipe Arruda <filipehenriquester@gmail.com>
2023-04-09 21:10:00 -0300

    - Atualizada versão do dotnet do publish-release-vps.yml - Criado arquivo RolePlayConfiguration.cs para gerenciar configuração de sistema - Removido yml do sonar

---
+++ Projects/UOContent/Custom/RolePlayConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Server.Logging;
+
+namespace Server.Custom;
+
+public class RolePlayConfiguration
+{
+    private static readonly ILogger logger = LogFactory.GetLogger(typeof(RolePlayConfiguration));
+
+    public static void Configure()
+    {
+        logger.Information("Carregando configurações da classe RolePlayConfiguration");
+
+        AgeSystemOn = ServerConfiguration.GetOrUpdateSetting("roleplay.ageSystemOn", true);
+        logger.Information($"ageSystemOn: {AgeSystemOn}");
+
+        FaintSystemOn = ServerConfiguration.GetOrUpdateSetting("roleplay.faintSystemOn", true);
+        logger.Information($"faintSystemOn: {FaintSystemOn}");
+
+        DynamicEconomySystemOn = ServerConfiguration.GetOrUpdateSetting("roleplay.dynamicEconomySystemOn", true);
+        logger.Information($"dynamicEconomySystemOn: {DynamicEconomySystemOn}");
+    }
+
+    public static bool DynamicEconomySystemOn { get; private set; }
+
+    public static bool FaintSystemOn { get; private set; }
+
+    public static bool AgeSystemOn { get; private set; }
+}
